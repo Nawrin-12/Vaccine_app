7a7b76962f985151b06910ed1c0c08e081215741
Imam Hossain <idealhossain@gmail.com>
2023-08-20 02:04:12 +0600

    ui changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 <div class="flex justify-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg">
-       Dashboard
-    </div>
-       </div>
-    </div>
+   <div class="w-8/12 bg-white p-6 rounded-lg">
+      <h1 style="font-size:24px; font-weight:700;">Dashboard</h1>
+
+      <p>User Name: <strong>{{ auth()->user()->username }}</strong></p>
+      <p>User email: <strong>{{ auth()->user()->email }}</strong></p>
+   </div>
 </div>
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lifey</title>
     @vite('resources/css/app.css')
+    <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+      margin: -10px; /* Negative margin to counteract spacing between columns */
+    }
+
+    .col {
+      flex: 0 0 calc(33.33% - 20px); /* Adjust the width based on your desired column layout */
+      margin: 10px; /* Spacing between columns */
+      box-sizing: border-box;
+      border: 1px solid #ccc;
+      padding: 20px;
+      transition: transform 0.2s, box-shadow 0.2s;
+      border-radius: 4px;
+    }
+
+    .col:hover {
+      transform: translateY(-5px);
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    .col h2 {
+        font-weight: 800;
+    }
+  </style>
 </head>
 <body class="bg-gray-200">
 <!-- <img src="{{ asset('images\vaccine.jpeg') }}" alt=""> -->
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href ="" class="p-3">Home</a>
+                <a href="{{ route('home') }}" class="p-3">Home</a>
             </li>
             <li>
-                <a href ="" class="p-3">Dashboard</a>
+                <a href ="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href ="{{ route('posts') }}" class="p-3">Query</a>
@@ -31,10 +67,6 @@
                     @csrf
                     <button type="submit">Logout</button>
                 </form>
-            </li>
-
-            <li>
-                <a href ="{{ route('requestvaccine.index') }}" class="p-3">Request Vaccine</a>
             </li>
             
             @endauth
